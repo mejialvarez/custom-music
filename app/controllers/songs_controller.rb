881e3783c662d6fc6fa0f39bdd14aeaf0727730b
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2016-11-14 17:35:50 -0500

    Adds root path

--- app/controllers/songs_controller.rb
+++
@@ -1,6 +0,0 @@
-class SongsController < ApplicationController
-  def index
-    fb_graph = current_user.fb_graph(session['facebook_access_token'])
-    byebug
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'spoty#index'
+
   get '/spoty' => 'spoty#index', as: 'spoty'
 
   devise_for :users, controllers: {
