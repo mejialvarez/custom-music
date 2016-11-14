9f5faa1ab931138defc3bfc2cedd25b32ac4d2e2
Mateo García <mateo1235@gmail.com>
2016-11-14 16:41:57 -0500

    agregadas pruebas unitarias al modelo Spoty

--- spec/models/spoty_spec.rb
+++ spec/models/spoty_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Spoty, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Probando el token de acceso a la API de Spotify" do
+    it "El token no es nulo" do
+      spoty = Spoty.new
+      token = spoty.gen_spotify_token
+      expect(token != nil)
+    end
+  end
+end
+
+RSpec.describe Spoty, type: :model do
+  context "Probando el token de acceso a la API de Spotify" do
+    it "El token tiene 86 caracteres" do
+      spoty = Spoty.new
+      token = spoty.gen_spotify_token
+      expect(token.size == 86)
+    end
+  end
+end
+
+RSpec.describe Spoty, type: :model do
+  context "Probando generacion de canciones desde Spotify" do
+    it "La lista no esta vacía" do
+      spoty = Spoty.new
+      token = spoty.gen_spotify_token
+      genres = ['rock', 'blues', 'reggaeton']
+      artists = ['rock', 'blues', 'reggaeton']
+      statuses = ['happy']
+      songs = spoty.gen_spotify_token
+      expect(songs != nil)
+    end
+  end
 end
