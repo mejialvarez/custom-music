69193d60e349d7f6f677c2165909e09fdbfa97a1
Mateo García <mateo1235@gmail.com>
2016-11-14 23:41:23 -0500

    agragda logica que usa los artistas para generar la lista de canciones desde spotify

--- app/controllers/spoty_controller.rb
+++ app/controllers/spoty_controller.rb
@@ -1,15 +1,23 @@
 class SpotyController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
     spoty = Spoty.new
     token = spoty.gen_spotify_token
-    spoty_genres = spoty.get_available_genres(token)
-    fb_genres = ['reggaeton', 'rock']
-    @genres = spoty.get_matched_genres(spoty_genres, fb_genres)
-    fb_artists = ['rock', 'blues', 'reggaeton']
+
+    # Facebook data seeds
+    fb_genres = ['salsa']
+    fb_artists = ['slipknot']
     fb_statuses = ['happy']
-    @songs = spoty.get_songs(token, @genres, fb_artists, fb_statuses)
+
+    # Spotify data seeds
+    spoty_genres = spoty.get_available_genres(token)
+    spotify_artists = spoty.get_artists_ids(token, fb_artists)
+    @genres = spoty.get_matched_genres(spoty_genres, fb_genres)
+    @artists = spoty.get_matched_artists(spotify_artists, fb_artists)
+
+    # Spotify query
+    @songs = spoty.get_songs(token, @genres, @artists, fb_statuses)
   end
 
 end
--- app/models/spoty.rb
+++ app/models/spoty.rb
@@ -2,6 +2,7 @@ class Spoty < ApplicationRecord
 
   require 'rest-client'
   require 'fuzzystringmatch'
+  require 'uri'
 
   # acousticness 0.0 - 1.0
   # danceability 0.0 - 1.0
@@ -33,20 +34,45 @@ class Spoty < ApplicationRecord
     results = Array.new
     songs = Array.new
     genres.each do | genre |
-      begin
-        res = RestClient.get 'https://api.spotify.com/v1/recommendations?seed_genres='+genre, {Authorization: 'Bearer '+token}
-      rescue Exception => e
-        return e.message
-        next
-      end
-      result = ActiveSupport::JSON.decode(res)
-      results.push(result)
-      results_songs = result['tracks']
-      results_songs.each do |song|
-        songs.push(song['id'])
+      artists.each do |artist, name|
+        begin
+          res = RestClient.get 'https://api.spotify.com/v1/recommendations?seed_genres='+genre+'&seed_artists='+artist, {Authorization: 'Bearer '+token}
+        rescue Exception => e
+          return e.message
+        end
+        result = ActiveSupport::JSON.decode(res)
+        results.push(result)
+        results_songs = result['tracks']
+        results_songs.each do |song|
+          songs.push(song['id'])
+        end
       end
     end
-    return songs.take(20)
+    return songs.sample(30)
+  end
+
+  # Codifica el texto para ser utilizado en URLs
+  def encode_text(text)
+    return URI.escape(text)
+  end
+
+  # Consulta los ids de los artistas en Spotify basado en una lista de artistas de Facebook
+  def get_artists_ids(token, artists)
+    artists_ids = Hash.new
+    artists.each do |artist|
+      artist_encoded = encode_text(artist)
+      begin
+        res = RestClient.get 'https://api.spotify.com/v1/search?q='+artist_encoded+'&type=artist', {Authorization: 'Bearer '+token}
+        result = ActiveSupport::JSON.decode(res)
+        artists_results = result['artists']['items']
+        artists_results.each do |ar|
+          artists_ids[ar['id']] = ar['name']
+        end
+      rescue Exception => e
+        return e.message
+      end
+    end
+    return artists_ids
   end
 
   # Consulta los generos disponibles con los que sepuede hacer consulta en Spotify
@@ -60,14 +86,33 @@ class Spoty < ApplicationRecord
     return result['genres']
   end
 
+  # Encuentra similitudes entre los artistas de Spotify y los de Facebook
+  def get_matched_artists(spotify_artists, fb_artists)
+    matched_artists = Hash.new
+    spotify_artists.each do |id, name|
+      fb_artists.each do |fa|
+        similarity = match_text(name, fa)
+        if similarity >= 0.9
+          matched_artists[id] = name
+        end
+      end
+    end
+    return matched_artists
+  end
+
+  # Determina la similitud entre dos strings
+  def match_text(text_one, text_two)
+    compare = FuzzyStringMatch::JaroWinkler.create(:native)
+    return compare.getDistance(text_one, text_two)
+  end
+
   # Encuentra similitudes entres lo generos de Spotify y los de Facebook y retorna una lista
   def get_matched_genres(spoty_genres, fb_genres)
     genres = Array.new
-    compare = FuzzyStringMatch::JaroWinkler.create(:native)
     spoty_genres.each do |sg|
       fb_genres.each do |fbg|
-        similarity = compare.getDistance(sg, fbg)
-        if similarity > 0.9
+        similarity = match_text(sg, fbg)
+        if similarity >= 0.9
           genres.push(sg)
         end
       end
