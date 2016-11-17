1387a3d705acd59cd97c12adec6c51a5a58db2c4
Mateo García <mateo1235@gmail.com>
2016-11-17 00:22:27 -0500

    refactor al modelo y a las pruebas. agregadas mas pruebas

--- app/controllers/spoty_controller.rb
+++ app/controllers/spoty_controller.rb
@@ -3,21 +3,14 @@ class SpotyController < ActionController::Base
 
   def index
     spoty = Spoty.new
-    token = spoty.gen_spotify_token
 
     # Facebook data seeds
-    fb_genres = ['salsa']
+    fb_genres = ['rock']
     fb_artists = ['slipknot']
     fb_statuses = ['happy']
 
-    # Spotify data seeds
-    spoty_genres = spoty.get_available_genres(token)
-    spotify_artists = spoty.get_artists_ids(token, fb_artists)
-    @genres = spoty.get_matched_genres(spoty_genres, fb_genres)
-    @artists = spoty.get_matched_artists(spotify_artists, fb_artists)
-
     # Spotify query
-    @songs = spoty.get_songs(token, @genres, @artists, fb_statuses)
+    @songs = spoty.get_songs(fb_genres, fb_artists, fb_statuses)
   end
 
 end
