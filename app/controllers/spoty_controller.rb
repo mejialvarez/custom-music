ff6848e64fbc851ce763db762cfaa2b046644efa
Daniel Robledo <danrax@hotmail.com>
2016-11-17 12:33:25 -0500

    Sin ENV

--- app/controllers/spoty_controller.rb
+++ app/controllers/spoty_controller.rb
@@ -1,26 +1,24 @@
 class SpotyController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action :set_fb_graph
 
   def index
     spoty = Spoty.new
-    user = User.new
-    token = spoty.gen_spotify_token
 
     # Facebook data seeds
-    fb_genres = ['salsa']
-    fb_artists = ['slipknot']
+    # fb_genres = ['rock']
+    # fb_artists = ['slipknot']
+
+    fb_genres = current_user.genres
+    fb_artists = current_user.artists
     fb_statuses = ['happy']
 
-    # Spotify data seeds
-    spoty_genres = spoty.get_available_genres(token)
-    spotify_artists = spoty.get_artists_ids(token, fb_artists)
-    @genres = spoty.get_matched_genres(spoty_genres, fb_genres)
-    @artists = spoty.get_matched_artists(spotify_artists, fb_artists)
-
     # Spotify query
-    @songs = spoty.get_songs(token, @genres, @artists, fb_statuses)
-    @u_name= current_user.name
-    @u_image= current_user.image
+    @songs = spoty.get_songs(fb_genres, fb_artists, fb_statuses)
   end
 
+  private
+    def set_fb_graph
+      current_user.set_fb_graph(session['fb_access_token'])
+    end
 end
