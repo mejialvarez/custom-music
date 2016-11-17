24e12fd0543a4d88f1de916e7922317e52f708bf
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2016-11-17 17:01:51 -0500

    Fix errors

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
 module CustomMusic
   class Application < Rails::Application
-    config.assets.version = '1.0'
-end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
